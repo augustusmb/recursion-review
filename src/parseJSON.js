2b3c01f8d75d758889f9404bdabfac1a79acf09e
augustusmb <augustusmb@gmail.com>
2017-02-06 17:39:47 -0800

    make progress on parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,72 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  
+
+  var output;
+  
+  var i = 0;
+
+  var cutQuotes = function(str) {
+    return str.slice(1, str.length - 1);
+  };  
+
+  var parseObject = function(obj) {
+    var split = obj.split(':');
+    var key = cutQuotes(split[0]);    
+    var value = parseJSON(cutQuotes(split[1]));
+    var object = new Object;
+
+    object[key] = value;
+    return object;
+  };
+
+  var parseArray = function(arr) {
+    var split = obj.split(',');
+    var array = new Array;
+    _.each(split, function(item) {
+      array.push(parseJSON(cutQuotes(item)));
+    });
+    return array;
+  };
+  
+  //array
+  if (json[i] === '[') {
+    if (json[i + 1] === ']') {
+      output = [];
+    } else {
+      output = parseArray(json.slice(1, json.length - 1));
+    }
+    //object 
+  } else if (json[i] === '{') {
+    if (json[i + 1] === '}') {
+      output = {};
+    } else {
+      output = parseObject(json.slice(1, json.length - 1));
+    }
+    } else if (parse)
+
+
+
+  // string
+  else {
+    output = json;
+  }
+  
+  return output;
 };
+
+
+  
+
+
+
+  // var parseNumber = function(obj) {
+
+  // };
+
+  
+
+  //   if () {
+  //     parseString(obj);
+  //   }
